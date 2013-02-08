c78e7d7fa86f0295d79b084853d881b165e33f43
Nolan Kelly <centhysakordu@gmail.com>
2013-02-08 16:11:02 -0700

    Created Test Project

---
+++ CarDealership/DBConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+
+namespace CarDealership
+{
+    class DBConnection
+    {
+        private OleDbConnection connection;
+        
+        public DBConnection(){
+
+            string ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=CarDealershipDatabase.accdb";
+
+            connection = new OleDbConnection(ConnectionString);
+            try
+            {
+                connection.Open();
+            }
+            catch (OleDbException ex)
+            {
+                Console.Error.WriteLine("Unable to connect to database.");
+                return;
+            }
+        }
+
+        public OleDbConnection GetDB()
+        {
+            return connection;
+        }
+
+    }
+}
---
+++ CarDealershipTests/SearchTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarDealership;
+
+
+namespace CarDealershipTests
+{
+    [TestClass]
+    public class SearchTests
+    {
+        [TestMethod]
+        public void Search_NormalPath()
+        {
+            DBConnection_Accessor connection = new DBConnection_Accessor();
+            connection.GetDB();
+            Assert.AreEqual("S", "S");
+        }
+    }
+}
