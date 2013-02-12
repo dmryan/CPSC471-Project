7893344c5edcab8a4c071376691c794fe6115e84
Nolan Kelly <centhysakordu@gmail.com>
2013-02-12 08:53:30 -0700

    More Tests

---
+++ CarDealershipTests/CarInventoryTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarDealership;
+using System.Data;
+using System.Data.OleDb;
+
+namespace CarDealershipTests
+{
+    [TestClass]
+    public class CarInventoryTests
+    {
+        [TestMethod]
+        public void CarInventory_NormalPath()
+        {
+            DBConnection_Accessor db = new DBConnection_Accessor();
+            StatsCalc_Accessor st = new StatsCalc_Accessor(db.GetDB());
+
+            DataTable dt = new DataTable();
+            dt = st.CarsInventory();
+            DataColumn[] dc = new DataColumn[] { dt.Columns[0] };
+            dt.PrimaryKey = dc;
+
+            Assert.IsTrue(dt.Rows.Contains("3"));
+
+        }
+
+        [TestMethod]
+        public void CarInventory_Sold()
+        {
+            DBConnection_Accessor db = new DBConnection_Accessor();
+            StatsCalc_Accessor st = new StatsCalc_Accessor(db.GetDB());
+
+            DataTable dt = new DataTable();
+            dt = st.CarsInventory();
+            DataColumn[] dc = new DataColumn[] { dt.Columns[0] };
+            dt.PrimaryKey = dc;
+
+            Assert.IsFalse(dt.Rows.Contains("1"));
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void CarInventory_Empty()
+        {
+            DBConnection_Accessor db = new DBConnection_Accessor();
+            StatsCalc_Accessor st = new StatsCalc_Accessor(db.GetDB());
+
+            DataTable dt = new DataTable();
+            dt = st.CarsInventory();
+            DataColumn[] dc = new DataColumn[] { dt.Columns[0] };
+            dt.PrimaryKey = dc;
+
+           
+            Assert.IsTrue(dt.Rows.Contains(" "));
+            
+
+        }
+    }
+}
---
+++ CarDealershipTests/RevenueTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarDealership;
+using System.Data;
+using System.Data.OleDb;
+
+namespace CarDealershipTests
+{
+    [TestClass]
+    public class RevenueTests
+    {
+        [TestMethod]
+        public void RevenueTest_OneSale()
+        {
+            DBConnection_Accessor db = new DBConnection_Accessor();
+            StatsCalc_Accessor st = new StatsCalc_Accessor(db.GetDB());
+
+            String s = st.Revenue();
+
+            Assert.IsTrue(int.Parse(s) > 0);
+        }
+
+        [TestMethod]
+        public void RevenueTest_NoSale()
+        {
+            DBConnection_Accessor db = new DBConnection_Accessor();
+            StatsCalc_Accessor st = new StatsCalc_Accessor(db.GetDB());
+
+            String s = st.Revenue();
+
+            Assert.IsTrue(int.Parse(s) == 0);
+        }
+
+        [TestMethod]
+        public void RevenueTest_MultipleSales()
+        {
+            DBConnection_Accessor db = new DBConnection_Accessor();
+            StatsCalc_Accessor st = new StatsCalc_Accessor(db.GetDB());
+
+            String s = st.Revenue();
+
+            Assert.IsTrue(int.Parse(s) > 0);
+        }
+
+        [TestMethod]
+        public void RevenueTest_DecimalSales()
+        {
+            DBConnection_Accessor db = new DBConnection_Accessor();
+            StatsCalc_Accessor st = new StatsCalc_Accessor(db.GetDB());
+
+            String s = st.Revenue();
+
+            Assert.IsTrue(double.Parse(s) > 0);
+        }
+    }
+}
